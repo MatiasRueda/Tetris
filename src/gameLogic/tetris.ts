fbf6166851ce960260dcd67f4a7a600c2af0c7db
MatiasRueda <matiasrued@gmail.com>
2024-06-25 10:38:23 -0300

    fix right move and add left move

--- src/gameLogic/tetris.ts
+++ src/gameLogic/tetris.ts
@@ -111,7 +111,30 @@ export default class Tetris {
     return true;
   }
 
+  private canMoveLeft() {
+    const rowFirstIndex = this.currentTetromino.typeRotation.rowFirstIndex;
+    const rowLastIndex = this.currentTetromino.typeRotation.rowLastIndex;
+    const colFirstIndex = this.currentTetromino.typeRotation.columnFirstIndex;
+    const firstRow = this.currentRow + rowFirstIndex;
+    const lastRow = this.currentRow + rowLastIndex;
+    const prevIndex = this.currentColumn + colFirstIndex - 1;
+    if (prevIndex < 0) return 1;
+    for (let row = firstRow; row <= lastRow; row++) {
+      if (row >= Tetris.HEIGHT) {
+        return 0;
+      }
+      if (
+        this.board[row][this.currentColumn + colFirstIndex] === 1 &&
+        this.board[row][prevIndex] === 1
+      ) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
   public moveTetrominoLeft(): boolean {
+    if (this.canMoveLeft() === 1) return false;
     const fistColumnIndex = this.currentTetromino.typeRotation.columnFirstIndex;
     if (this.currentColumn - 1 + fistColumnIndex < 0) return false;
     this.removeCurrentTetromino();
@@ -127,7 +150,8 @@ export default class Tetris {
     const firstRow = this.currentRow + rowFirstIndex;
     const lastRow = this.currentRow + rowLastIndex;
     const nextIndex = this.currentColumn + colLastIndex + 1;
-    if (nextIndex >= Tetris.WIDTH) return false;
+    console.log(nextIndex);
+    if (nextIndex >= Tetris.WIDTH) return 1;
     for (let row = firstRow; row <= lastRow; row++) {
       if (row >= Tetris.HEIGHT) {
         return 0;
@@ -143,9 +167,7 @@ export default class Tetris {
   }
 
   public moveTetrominoRight() {
-    if (this.canMoveRight() === 1) {
-      return false;
-    }
+    if (this.canMoveRight() === 1) return false;
     this.removeCurrentTetromino();
     this.currentColumn += 1;
     this.addTetromino();
