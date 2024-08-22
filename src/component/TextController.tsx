efb30965fec833f21445a73931378b346656fdfc
MatiasRueda <matiasrued@gmail.com>
2024-08-22 04:11:14 -0300

    add styles

--- src/component/TextController.tsx
+++ src/component/TextController.tsx
@@ -2,10 +2,12 @@ export default function TextController() {
   return (
     <div className="cont-text">
       <h3>Controllers:</h3>
-      <p>- Press W to rotate</p>
-      <p>- Press A to move left</p>
-      <p>- Press D to move right</p>
-      <p>- Press S to move down</p>
+      <div>
+        <p>- Press W to rotate</p>
+        <p>- Press A to move left</p>
+        <p>- Press D to move right</p>
+        <p>- Press S to move down</p>
+      </div>
     </div>
   );
 }
--- src/screens/Loading.tsx
+++ src/screens/Loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-  return <h1 style={{ color: "#ffff", fontSize: "1.2em" }}>Loading...</h1>;
+  return <h1 style={{ color: "#ffff", fontSize: "1.5em" }}>Loading...</h1>;
 }
